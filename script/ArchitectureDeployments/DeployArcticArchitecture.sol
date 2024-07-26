7bc8ead218f12d2d45453b04a7541a5730d49884
crispymangoes <crispymangoes@gmail.com>
2024-07-26 10:01:52 -0700

    Remove Solver Role setup from deploy script

--- script/ArchitectureDeployments/DeployArcticArchitecture.sol
+++ script/ArchitectureDeployments/DeployArcticArchitecture.sol
@@ -114,7 +114,6 @@ contract DeployArcticArchitecture is Script, ContractNames {
     uint8 public constant MINTER_ROLE = 2;
     uint8 public constant BURNER_ROLE = 3;
     uint8 public constant MANAGER_INTERNAL_ROLE = 4;
-    uint8 public constant SOLVER_ROLE = 12;
     uint8 public constant OWNER_ROLE = 8;
     uint8 public constant MULTISIG_ROLE = 9;
     uint8 public constant STRATEGIST_MULTISIG_ROLE = 10;
@@ -357,25 +356,6 @@ contract DeployArcticArchitecture is Script, ContractNames {
                     address(manager),
                     ManagerWithMerkleVerification.manageVaultWithMerkleVerification.selector,
                     true
-                );
-            }
-            // SOLVER_ROLE
-            if (
-                !rolesAuthority.doesRoleHaveCapability(
-                    SOLVER_ROLE, address(teller), TellerWithMultiAssetSupport.bulkDeposit.selector
-                )
-            ) {
-                rolesAuthority.setRoleCapability(
-                    SOLVER_ROLE, address(teller), TellerWithMultiAssetSupport.bulkDeposit.selector, true
-                );
-            }
-            if (
-                !rolesAuthority.doesRoleHaveCapability(
-                    SOLVER_ROLE, address(teller), TellerWithMultiAssetSupport.bulkWithdraw.selector
-                )
-            ) {
-                rolesAuthority.setRoleCapability(
-                    SOLVER_ROLE, address(teller), TellerWithMultiAssetSupport.bulkWithdraw.selector, true
                 );
             }
             // OWNER_ROLE
