45d735fd04907ded586d7e1e9778dbd473cab63d
shawn <mantle.xyz>
2024-09-11 12:11:57 +1000

    [fix]: migrate cmETH token audits

--- src/lists/SanctionsList.sol
+++ src/lists/SanctionsList.sol
@@ -3,12 +3,11 @@
 */
 pragma solidity 0.8.20;
 
-import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {IISanctionsList} from "../../src/interfaces/ITransferSanctionList.sol";
 
 // @notice we will use Chainalysis's SanctionsList(eth:0x40C57923924B5c5c5455c48D93317139ADDaC8fb)
 // @dev contract above is only for testing
-contract SanctionsList is IISanctionsList, Ownable {
+contract SanctionsList is IISanctionsList {
 
     constructor() {}
 
